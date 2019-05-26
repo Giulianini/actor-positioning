3b089c50df26d7cdbd3c77534a40fc31752cb930
Manuel Bonarrigo <manuel.bonarrigo@studio.unibo.it>
2019-05-26 17:09:59 +0200

    fix

--- src/main/scala/it/unibo/pcd1819/actorpositioning/Main.scala
+++ src/main/scala/it/unibo/pcd1819/actorpositioning/Main.scala
@@ -3,6 +3,7 @@ package it.unibo.pcd1819.actorpositioning
 import akka.actor.{Actor, ActorSystem, DeadLetter, Props}
 import com.typesafe.config.ConfigFactory
 import it.unibo.pcd1819.actorpositioning.actors.ControllerFSM
+import it.unibo.pcd1819.actorpositioning.model.{Particle, Vector2D}
 
 class DeadLetterListener extends Actor {
   override def receive: Receive = {
@@ -14,6 +15,11 @@ object Main extends App {
   val system = ActorSystem("root", ConfigFactory.parseString("""akka.loglevel = "DEBUG""""))
   system.eventStream.subscribe(system.actorOf(Props(classOf[DeadLetterListener])), classOf[DeadLetter])
 
+//  val result = Seq(Particle(Vector2D(20, 20), 60, 60)(6))
+
   val controller = system actorOf(ControllerFSM.props, "controller")
+
+//  Thread.sleep(5000)
+//  controller ! ControllerFSM.Result(result)
 
 }
--- src/main/scala/it/unibo/pcd1819/actorpositioning/actors/ViewActor.scala
+++ src/main/scala/it/unibo/pcd1819/actorpositioning/actors/ViewActor.scala
@@ -3,7 +3,7 @@ package it.unibo.pcd1819.actorpositioning.actors
 import akka.actor.{Actor, ActorLogging, Props}
 import com.sun.javafx.application.PlatformImpl
 import it.unibo.pcd1819.actorpositioning.actors.ViewActor.Publish
-import it.unibo.pcd1819.actorpositioning.model.Environment
+import it.unibo.pcd1819.actorpositioning.model.{Environment, Particle}
 import it.unibo.pcd1819.actorpositioning.view.screens.ViewToActorMessages._
 import it.unibo.pcd1819.actorpositioning.view.screens.{ActorObserver, MainScreenView}
 
@@ -13,13 +13,13 @@ class ViewActor extends Actor with ActorLogging {
   screenView.setViewActorRef(self)
 
   override def receive: Receive = {
-    case Publish(e) => screenView.displayParticles(e.environment)
+    case Publish(e) => screenView.displayParticles(e)
 
     case StartSimulation => context.parent ! ControllerFSM.Start
     case PauseSimulation => context.parent ! ControllerFSM.Pause
     case StopSimulation => context.parent ! ControllerFSM.Stop
     case StepSimulation => context.parent ! ControllerFSM.Step
-    case PrepareSimulation => context.parent ! ControllerFSM.Generate
+    case PrepareSimulation => context.parent ! ControllerFSM.GenerateEnvironment
     case SetParticle(amount: Int) => log debug s"Set particles $amount"
     case SetIteration(amount: Int) => log debug s"Set iteration $amount"
     case SetTime(amount: Int) => log debug s"Set time $amount"
@@ -30,6 +30,6 @@ class ViewActor extends Actor with ActorLogging {
 
 object ViewActor {
   def props = Props(new ViewActor())
-  final case class Publish(env: Environment)
+  final case class Publish(env: Seq[Particle])
   final case object Stop
 }
