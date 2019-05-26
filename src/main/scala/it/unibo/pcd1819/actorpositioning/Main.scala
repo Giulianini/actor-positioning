caec12ffacdfcfa760bd327e0aa25d6745252a92
Manuel Bonarrigo <manuel.bonarrigo@studio.unibo.it>
2019-05-26 16:36:31 +0200

    maybe finished controller

--- src/main/scala/it/unibo/pcd1819/actorpositioning/Main.scala
+++ src/main/scala/it/unibo/pcd1819/actorpositioning/Main.scala
@@ -2,17 +2,18 @@ package it.unibo.pcd1819.actorpositioning
 
 import akka.actor.{Actor, ActorSystem, DeadLetter, Props}
 import com.typesafe.config.ConfigFactory
-import it.unibo.pcd1819.actorpositioning.actors.{ControllerFSM, ViewActor}
+import it.unibo.pcd1819.actorpositioning.actors.{ControllerFSM, ParticleFactoryActor, ViewActor}
+import it.unibo.pcd1819.actorpositioning.model.{Environment, Particle, Vector2D}
 
 class DeadLetterListener extends Actor {
-    override def receive: Receive = {
-        case d: DeadLetter => println("DeadLetter: " + d.message + " by " + d.sender + " aimed at " + d.recipient)
-    }
+  override def receive: Receive = {
+    case d: DeadLetter => println("DeadLetter: " + d.message + " by " + d.sender + " aimed at " + d.recipient)
+  }
 }
 
 object Main extends App {
-    val system = ActorSystem("root", ConfigFactory.parseString("""akka.loglevel = "DEBUG""""))
-    system.eventStream.subscribe(system.actorOf(Props(classOf[DeadLetterListener])), classOf[DeadLetter])
+  val system = ActorSystem("root", ConfigFactory.parseString("""akka.loglevel = "DEBUG""""))
+  system.eventStream.subscribe(system.actorOf(Props(classOf[DeadLetterListener])), classOf[DeadLetter])
 
-    val controller = system actorOf(ControllerFSM.props, "controller")
+  val controller = system actorOf(ControllerFSM.props, "controller")
 }
--- src/main/scala/it/unibo/pcd1819/actorpositioning/actors/ControllerFSM.scala
+++ src/main/scala/it/unibo/pcd1819/actorpositioning/actors/ControllerFSM.scala
@@ -26,9 +26,9 @@ object ControllerFSM {
   final case class UpdateTimeStep(n: Int) extends Input
   final case class Add(x: Double, y: Double) extends Input
   final case class Remove(p: Particle) extends Input
-  final case class Result(e: Environment) extends Input
+  final case class Result(e: Seq[Particle]) extends Input
 
-  private final case class Settings(particles: Int, iterations: Int, timeStep: Int)
+  final case class Settings(particles: Int, iterations: Int, timeStep: Int)
   def props = Props(classOf[ControllerFSM])
 }
 
@@ -46,39 +46,39 @@ class ControllerFSM extends FSM[State, Data] with ActorLogging{
     case Event(Step, _) => goto(Paused)
     case Event(Pause, _) => goto(Pit)
     case Event(Stop, _) => goto(Pit)
-    case Event(GenerateEnvironment, Request(s:_, _)) => goto(Idle).using(Request(s, GenerateEnvironment))
-    case Event(UpdateParticles(n), Request(s:_, _)) => goto(Idle).using(Request(s.copy(particles = n), UpdateParticles(n)))
-    case Event(UpdateIterations(n), Request(s:_, _)) => goto(Idle).using(Request(s.copy(iterations = n), UpdateIterations(n)))
-    case Event(UpdateTimeStep(n), Request(s:_, _)) => goto(Idle).using(Request(s.copy(timeStep = n), UpdateTimeStep(n)))
-    case Event(Add(x, y), Request(s:_, _)) => goto(Idle).using(Request(s, Add(x, y)))
-    case Event(Remove(p), Request(s:_, _)) => goto(Idle).using(Request(s, Remove(p)))
-    case Event(Result(e), Request(s:_, _)) => goto(Idle).using(Request(s, Result(e)))
+    case Event(GenerateEnvironment, Request(s, _)) => goto(Idle).using(Request(s, GenerateEnvironment))
+    case Event(UpdateParticles(n), Request(s, _)) => goto(Idle).using(Request(s.copy(particles = n), UpdateParticles(n)))
+    case Event(UpdateIterations(n), Request(s, _)) => goto(Idle).using(Request(s.copy(iterations = n), UpdateIterations(n)))
+    case Event(UpdateTimeStep(n), Request(s, _)) => goto(Idle).using(Request(s.copy(timeStep = n), UpdateTimeStep(n)))
+    case Event(Add(x, y), Request(s, _)) => goto(Idle).using(Request(s, Add(x, y)))
+    case Event(Remove(p), Request(s, _)) => goto(Idle).using(Request(s, Remove(p)))
+    case Event(Result(e), Request(s, _)) => goto(Idle).using(Request(s, Result(e)))
   }
 
   when(Running) {
     case Event(Start, _) => goto(Pit)
-    case Event(Step, Request(s:_, _)) => goto(Running).using(Request(s, Step))
+    case Event(Step, Request(Settings(p, i, t), _)) => goto(Running).using(Request(Settings(p, i-1, t), Step))
     case Event(Pause, _) => goto(Paused)
     case Event(Stop, _) => goto(Idle)
     case Event(GenerateEnvironment, _) => goto(Pit)
     case Event(UpdateIterations(_), _) => goto(Pit)
     case Event(UpdateTimeStep(_), _) => goto(Pit)
-    case Event(Add(x, y), Request(s:_, _)) => goto(Running).using(Request(s, Add(x, y)))
-    case Event(Remove(p), Request(s:_, _)) => goto(Running).using(Request(s, Remove(p)))
-    case Event(Result(e), Request(s:_, _)) => goto(Running).using(Request(s, Result(e)))
+    case Event(Add(x, y), Request(s, _)) => goto(Running).using(Request(s, Add(x, y)))
+    case Event(Remove(p), Request(s, _)) => goto(Running).using(Request(s, Remove(p)))
+    case Event(Result(e), Request(s, _)) => goto(Running).using(Request(s, Result(e)))
   }
 
   when(Paused) {
     case Event(Start, _) => goto(Running)
-    case Event(Step, _) => goto(Paused)
+    case Event(Step, Request(Settings(p, i, t), _)) => goto(Paused).using(Request(Settings(p, i-1, t), Step))
     case Event(Pause, _) => goto(Pit)
     case Event(Stop, _) => goto(Idle)
     case Event(GenerateEnvironment, _) => goto(Pit)
     case Event(UpdateIterations(_), _) => goto(Pit)
     case Event(UpdateTimeStep(_), _) => goto(Pit)
-    case Event(Add(x, y), Request(s:_, _)) => goto(Paused).using(Request(s, Add(x,y)))
-    case Event(Remove(p), Request(s:_, _)) => goto(Paused).using(Request(s, Remove(p)))
-    case Event(Result(e), Request(s:_, _)) => goto(Paused).using(Request(s, Result(e)))
+    case Event(Add(x, y), Request(s, _)) => goto(Paused).using(Request(s, Add(x,y)))
+    case Event(Remove(p), Request(s, _)) => goto(Paused).using(Request(s, Remove(p)))
+    case Event(Result(e), Request(s, _)) => goto(Paused).using(Request(s, Result(e)))
   }
 
   when(Pit) {
@@ -91,9 +91,9 @@ class ControllerFSM extends FSM[State, Data] with ActorLogging{
 
     case Idle -> Idle =>
       nextStateData match {
-        case Request(s:_, GenerateEnvironment) =>
+        case Request(Settings(p, _, _), GenerateEnvironment) =>
           log debug s"Idle asked to generate environment"
-          environment ! EnvironmentActor.Generate(s.particles, EnvironmentConstants.radius)
+          environment ! EnvironmentActor.Generate(p, EnvironmentConstants.radius)
         case Request(_, Add(x, y)) =>
           log debug s"Idle asked for creation of $x, $y"
           environment ! EnvironmentActor.Add(x, y)
@@ -130,10 +130,12 @@ class ControllerFSM extends FSM[State, Data] with ActorLogging{
           log debug s"Running sent the last environment to be published and is going to shutdown"
           view ! ViewActor.Publish(e)
           self ! ControllerFSM.Stop
-        case Request(_, Result(e)) =>
+        case Request(Settings(_, i, _), Result(e)) =>
           log debug s"Running asked to publish $e and to perform an additional Step"
           view ! ViewActor.Publish(e)
-          environment ! EnvironmentActor.Step
+          self ! ControllerFSM.Step
+        case Request(Settings(_, i, _), Step) =>
+          log debug s"Remaining iterations: $i"
         case _ => log debug "Report this message ASAP"
       }
     case Running -> Idle =>
@@ -156,8 +158,8 @@ class ControllerFSM extends FSM[State, Data] with ActorLogging{
         case Request(Settings(_, 0, _), Step) =>
           log debug "Paused can no longer perform any Step and is going to shutdown"
           self ! ControllerFSM.Stop
-        case Request(_, Step) =>
-          log debug s"Paused asked to perform another Step"
+        case Request(Settings(_, i, _), Step) =>
+          log debug s"Paused asked to perform another Step, remaining iterations: $i"
           environment ! EnvironmentActor.Step
         case _ => log debug "Report this message ASAP"
       }
