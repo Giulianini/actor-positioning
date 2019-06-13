58bf3b9855aa35b3f118d7f33bd9ed7571bbbcbb
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2019-06-13 16:17:52 +0200

    Pulish Main

--- src/main/scala/it/unibo/pcd1819/actorpositioning/Main.scala
+++ src/main/scala/it/unibo/pcd1819/actorpositioning/Main.scala
@@ -6,7 +6,7 @@ import it.unibo.pcd1819.actorpositioning.actors.ControllerFSM
 
 class DeadLetterListener extends Actor {
   override def receive: Receive = {
-    case d: DeadLetter => //println("DeadLetter: " + d.message + " by " + d.sender + " aimed at " + d.recipient)
+    case d: DeadLetter =>
   }
 }
 
@@ -14,10 +14,5 @@ object Main extends App {
   val conf = ConfigFactory.load()
   val system = ActorSystem("root", conf)
   system.eventStream.subscribe(system.actorOf(Props(classOf[DeadLetterListener])), classOf[DeadLetter])
-
   val controller = system actorOf(ControllerFSM.props, "controller")
-  //val result = mutable.MutableList[Particle](Particle(Vector2D(20, 20), 60, 60, 1))
-  //Thread.sleep(5000)
-  //controller ! ControllerFSM.Result(result)
-
 }
